b8af368b8a5121c1a7bf98da4a4f5c2508f9379c
Perrin Antonin <tom.per@orange.fr>
2019-05-20 23:32:14 +0200

    chargement des données

--- model.py
+++ model.py
@@ -14,43 +14,56 @@ from sklearn.model_selection import train_test_split
 from tensorflow.python.keras.models import Model
 from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import Conv2D, Dense, Flatten, Cropping2D, Lambda, Dropout
+from tensorflow.python import keras
 
 
-DATA_PATH = "data/driving_log.csv"
+DATA_PATH = "data/"
 DATA_IMG = "data/"
 
-def build_model():
+# création du reseaux convolutif
+class ConvModel(keras.Model):
+    def __init__(self):
+        super(ConvModel, self).__init__()
+        # Convolutions
+        self.alea = Lambda(lambda x: x/127.5-1.0, input_shape=(320*160 Inpu))
+        self.conv1 = keras.layers.Conv2D(32, 4, activation='relu', name="conv1")
+        self.conv2 = keras.layers.Conv2D(64, 3, activation='relu', name="conv2")
+        self.conv3 = keras.layers.Conv2D(128, 3, activation='relu', name="conv3")
 
-    model = Sequential()
-    model.add(Lambda(lambda x: x/127.5-1.0, input_shape=INPUT_SHAPE))
-    model.add(Conv2D(24, kernel_size=(5, 5),strides=(2,2) ,activation='elu'))
-    model.add(Conv2D(36, kernel_size=(5, 5),strides=(2,2) ,activation='elu'))
-    model.add(Conv2D(48, kernel_size=(5, 5),strides=(2,2),activation='elu'))
-    model.add(Conv2D(64, kernel_size=(3, 3), activation='elu'))
-    model.add(Conv2D(64, kernel_size=(3, 3), activation='elu'))
-    model.add(Dropout(0.5))
-    model.add(Flatten())
-    model.add(Dense(100, activation='elu'))
-    model.add(Dense(50, activation='elu'))
-    model.add(Dense(10, activation='elu'))
-    model.add(Dense(1))
-    #model.summary()
-    return model
+        self.pool1 = keras.layers.MaxPooling2D((2, 2))
+        model.add(keras.layers.Conv2D(64, (3, 3), activation='relu'))
+        model.add(keras.layers.MaxPooling2D((2, 2)))
+        model.add(keras.layers.Conv2D(64, (3, 3), activation='relu'))
+        model.summary()
+
+        model.add(keras.layers.Flatten())
+        model.add(keras.layers.Dense(64, activation='relu'))
+        model.add(keras.layers.Dense(10, activation='softmax'))
+        model.summary()
+        # Flatten the convolution
+        self.flatten = keras.layers.Flatten(name="flatten")
+        # Dense layers
+        self.d1 = keras.layers.Dense(128, activation='relu', name="d1")
+        self.out = keras.layers.Dense(10, activation='softmax', name="output")
+
+    def call(self, image):
+        conv1 = self.conv1(image)
+        conv2 = self.conv2(conv1)
+        conv3 = self.conv3(conv2)
+        flatten = self.flatten(conv3)
+        d1 = self.d1(flatten)
+        output = self.out(d1)
+        return output
 
 
 def load_data():
-    data_dir ="C:\\Users\\tompe\\Documents\\VoitureAutonome\\data"
-    data_df = pd.read_csv(os.path.join(os.getcwd(),data_dir, 'driving_log.csv'), names=['center', 'left', 'right', 'steering', 'throttle', 'reverse', 'speed'])
+    data_df = pd.read_csv(os.path.join(os.getcwd(),DATA_PATH, 'driving_log.csv'), names=['center', 'left', 'right', 'steering', 'throttle', 'reverse', 'speed'])
     X = data_df[['center', 'left', 'right']].values
     y = data_df['steering'].values
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
     return X_train, X_test, y_train, y_test
 
-def preprocess(image):
-    image = crop(image)
-    image = resize(image)
-    image = rgb2yuv(image)
-    return image
+
 
 
 def main():
@@ -58,23 +71,11 @@ def main():
         Main function to train the model
     """
     data = load_data()
-    print("test")
-    steering_angle = float(data["steering_angle"])
-    throttle = float(data["throttle"])
-    speed = float(data["speed"])
-    image = Image.open(BytesIO(base64.b64decode(data["image"])))
-    image = np.asarray(image)
-    image = preprocess_data.preprocess(image)
-    image = np.array([image])
+    print(data[3])
 
-    steering_angle = float(model.predict(image, batch_size=1))
-    throttle = 1.0 - steering_angle ** 2 - (speed / speed_limit) ** 2
-
-    # send prediction to the simulator
-    send_control(steering_angle, throttle)
 
 
 if __name__ == '__main__':
     main()
-    print('test')
+
 
