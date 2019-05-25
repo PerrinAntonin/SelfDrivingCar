e6aa95687d6a1ccd04c5d2cec67ecd26d2170d55
Perrin Antonin <tom.per@orange.fr>
2019-05-25 12:09:15 +0200

    mis en place du model

--- model.py
+++ model.py
@@ -35,40 +35,35 @@ def load_data():
 def get_data(batch_size,imageData, rotationData):
     print("load data...")
     rotations = []
-    images = np.empty(1)
-    print(images)
-    angle_correction = [0., 0.25, -.25]
-    for index in range(0,100):
-        i = random.choice([0, 1, 2]) # [Center, Left, Right]
-        print("testtest",x_train[index][i])
-        img = cv2.imread(os.path.join(DATA_IMG,x_train[index][i]).replace(" ", ""))
+    images = []
+    
+    angle_correction = [0., 0.25, -.25] # [Center, Left, Right]
+
+    for index in range(0,batch_size):
+        i = random.choice([0, 1, 2]) # for direction
+
+        img = cv2.imread(os.path.join(DATA_IMG,imageData[index][i]).replace(" ", ""))
         if img is None: 
             continue
 
-        print(os.path.join(DATA_IMG,x_train[index][i]))
         #converti la couleur de l'image
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        #recupere la rotation A VERIFIER SI LES VALEUR RECUPERER CORRESPOND
+        #img = img.astype(np.float32)
+        #recupere la rotation 
         if index%3==0:
             subIndex=index//3
             rotation = float(rotationData[subIndex])
-            #print("wi",rotation)
 
-            #ajou de la correction
-        #print("index:",index)
-        #print("before",rotation)
+        #ajou de la correction
         rotation = rotation + angle_correction[i]
-        print("after",rotation)
-        #ajou des informations a la liste
-        images = np.append(images,img)
-        rotations = np.append(rotations,rotation)
-        #images.append(img)
-        #rotations.append(rotation)
-        #print(images)
 
-        if len(images) >= batch_size:
-            print("end loading",len(images))
-            return images, rotations
+        #ajou des informations a la liste
+        images.append(img)
+        rotations.append(rotation)
+
+    print("end loading longeur image:",len(images))
+
+    return np.array(images), np.array(rotations)
 
 # création du reseaux convolutif
 class ConvModel(keras.Model):
@@ -76,34 +71,34 @@ class ConvModel(keras.Model):
         super(ConvModel, self).__init__()
         # Convolutions
         self.alea = Lambda(lambda x: (x/127)-1, input_shape=(160,320,3))
-        self.conv1_1 = keras.layers.Conv2D(32, 4, activation='relu', name="conv1_1")
-        self.conv1_2 = keras.layers.Conv2D(64, 3, activation='relu', name="conv1_2")
+        self.conv1_1 = keras.layers.Conv2D(64, (3,3), activation='relu', name="conv1_1")
+        self.conv1_2 = keras.layers.Conv2D(64, (3,3), activation='relu', name="conv1_2")
         self.pool1 = keras.layers.MaxPooling2D((2, 2))
 
-        self.conv2_1 = keras.layers.Conv2D(128, 3, activation='relu', name="conv2_1")
-        self.conv2_2 = keras.layers.Conv2D(128, 3, activation='relu', name="conv2_2")
+        self.conv2_1 = keras.layers.Conv2D(128, (3,3), activation='relu', name="conv2_1")
+        self.conv2_2 = keras.layers.Conv2D(128, (3,3), activation='relu', name="conv2_2")
         self.pool2 = keras.layers.MaxPooling2D((2, 2))
 
-        self.conv3_1 = keras.layers.Conv2D(128, 3, activation='relu', name="conv3_1")
-        self.conv3_2 = keras.layers.Conv2D(128, 3, activation='relu', name="conv3_2")
-        self.conv3_3 = keras.layers.Conv2D(128, 3, activation='relu', name="conv3_3")
+        self.conv3_1 = keras.layers.Conv2D(256, (3,3), activation='relu', name="conv3_1")
+        self.conv3_2 = keras.layers.Conv2D(256, (3,3), activation='relu', name="conv3_2")
+        self.conv3_3 = keras.layers.Conv2D(256, (3,3), activation='relu', name="conv3_3")
         self.pool3 = keras.layers.MaxPooling2D((2, 2))
 
-        self.conv4_1 = keras.layers.Conv2D(128, 3, activation='relu', name="conv4_1")
-        self.conv4_2 = keras.layers.Conv2D(128, 3, activation='relu', name="conv4_2")
-        self.conv4_3 = keras.layers.Conv2D(128, 3, activation='relu', name="conv4__3")
+        self.conv4_1 = keras.layers.Conv2D(512, (2,2), activation='relu', name="conv4_1")
+        self.conv4_2 = keras.layers.Conv2D(512, (2,2), activation='relu', name="conv4_2")
+        self.conv4_3 = keras.layers.Conv2D(512, (2,2), activation='relu', name="conv4_3")
         self.pool4 = keras.layers.MaxPooling2D((2, 2))
-
-        self.conv5_1 = keras.layers.Conv2D(128, 3, activation='relu', name="conv5_1")
-        self.conv5_2 = keras.layers.Conv2D(128, 3, activation='relu', name="conv5_2")
-        self.conv5_3 = keras.layers.Conv2D(128, 3, activation='relu', name="conv5_3")
+        
+        self.conv5_1 = keras.layers.Conv2D(512, (2,2), activation='relu', name="conv5_1")
+        self.conv5_2 = keras.layers.Conv2D(512, (2,2), activation='relu', name="conv5_2")
+        self.conv5_3 = keras.layers.Conv2D(512, (2,2), activation='relu', name="conv5_3")
         self.pool5 = keras.layers.MaxPooling2D((2, 2))
 
         # Flatten the convolution
         self.flatten = keras.layers.Flatten(name="flatten")
         # Add layers
-        self.d1 = keras.layers.Dense(256, activation='relu', name="d1")
-        self.d2 = keras.layers.Dense(128, activation='relu', name="d2")
+        self.d1 = keras.layers.Dense(4096, activation='relu', name="d1")
+        self.d2 = keras.layers.Dense(2048, activation='relu', name="d2")
         self.out = keras.layers.Dense(1, activation='softmax', name="output")
         
 
@@ -114,7 +109,7 @@ class ConvModel(keras.Model):
         pool1 =self.pool1(conv1_2)
                 
         conv2_1 = self.conv2_1(pool1)
-        conv2_2 = self.conv2_2(conv1_1)
+        conv2_2 = self.conv2_2(conv2_1)
         pool2 =self.pool2(conv2_2)
         
         conv3_1 = self.conv3_1(pool2)
@@ -130,7 +125,7 @@ class ConvModel(keras.Model):
         conv5_1 = self.conv5_1(pool4)
         conv5_2 = self.conv5_2(conv5_1)
         conv5_3 = self.conv5_3(conv5_2)
-        pool5 =self.pool1(conv5_3)
+        pool5 =self.pool5(conv5_3)
 
         flatten = self.flatten(pool5)
         d1 = self.d1(flatten)
@@ -139,20 +134,17 @@ class ConvModel(keras.Model):
         return output
 
 
-
-
 data = load_data()
 
 model = ConvModel()
 
 x_train = data[0]
-y_train = data[2]
 x_valid = data[1]
+y_train = data[2]
 y_valid = data[3]
-print("xtrain",x_train)
 
-images, rotations = get_data(20,x_train,y_train)
-images_valid, rotations_valid = get_data(20,x_valid,y_valid)
+images, rotations = get_data(32,x_train,y_train)
+images_valid, rotations_valid = get_data(32,x_valid,y_valid)
 
 # conversion des images de float 64 en 32 car con2d veut du 32
 #images = images.astype(np.float32)
@@ -168,8 +160,8 @@ rotations = np.array(rotations)
 print("rotation shape",rotations.shape)
 images_valid=np.array(images_valid)
 print("image before shape",images.shape)
-images = np.reshape(images,(-1, 160,320))
-images_valid = np.reshape(images_valid,(-1, 160,320))
+images = np.reshape(images,(-1, 160,320,3))
+images_valid = np.reshape(images_valid,(-1, 160,320,3))
 print("image after shape",images.shape)
 
 # Normalisation
@@ -178,19 +170,21 @@ scaler = StandardScaler()
 scaled_images = scaler.fit_transform(images.reshape(-1, 160*320))
 scaled_images_valid = scaler.transform(images_valid.reshape(-1, 160*320))
 print("Moyenne et ecart type des images normalisé", scaled_images.mean(), scaled_images.std())
+print("after normalisation",rotations.shape)
+print("after normalisation",scaled_images.shape)
 
-
-scaled_images = scaled_images.reshape(-1, 160, 320, 1)
-scaled_images_valid = scaled_images_valid.reshape(-1, 160, 320, 1)
+scaled_images = scaled_images.reshape(-1, 160,320,3)
+scaled_images_valid = scaled_images_valid.reshape(-1, 160,320,3)
 print("after scaled",rotations.shape)
-print("after scaled",images.shape)
+print("after scaled",scaled_images.shape)
+
 train_dataset = tf.data.Dataset.from_tensor_slices((scaled_images, rotations))
 valid_dataset = tf.data.Dataset.from_tensor_slices((scaled_images_valid, rotations_valid))
 
 
-BATCH_SIZE = 64
+BATCH_SIZE = 32
 
-loss_object = tf.keras.losses.SparseCategoricalCrossentropy()
+loss_object = keras.losses.SparseCategoricalCrossentropy()
 optimizer = tf.keras.optimizers.Adam()
 #track the evolution
 # Loss
@@ -201,13 +195,16 @@ train_accuracy = metrics.SparseCategoricalAccuracy(name='train_accuracy')
 valid_accuracy = metrics.SparseCategoricalAccuracy(name='valid_accuracy')
 
 
+
 @tf.function
 def train_step(image, targets):
-    print(targets)
     # permet de surveiller les opérations réalisé afin de calculer le gradient
     with tf.GradientTape() as tape:
         # fait une prediction
         predictions = model(image)
+
+        print("after creation model targets shape",targets)
+        print("after creation model prediction shape",predictions)
         # calcul de l'erreur en fonction de la prediction et des targets
         loss = loss_object(targets, predictions)
     # calcul du gradient en fonction du loss
@@ -232,14 +229,10 @@ def valid_step(image, targets):
 epoch = 10
 batch_size = 32
 b = 0
-print(train_dataset.batch(batch_size))
+
 for epoch in range(epoch):
     # Training set
     for (images_batch, targets_batch) in train_dataset.batch(batch_size):
-        print(images_batch)
-        print(targets_batch)
-        print("shape of images batch",images_batch.shape)
-
         train_step(images_batch, targets_batch)
         template = '\r Batch {}/{}, Loss: {}, Accuracy: {}'
         print(template.format(
