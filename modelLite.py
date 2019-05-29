4be234cba980c30719f0bbe3891da5ead4684723
Perrin Antonin <tom.per@orange.fr>
2019-05-29 19:05:22 +0200

    update model et changement de fonction de loss/accuracy plus petit update

--- modelLite.py
+++ modelLite.py
@@ -18,10 +18,6 @@ from tensorflow.python.keras import metrics, optimizers, losses
 import tensorflow as tf
 
 
-from tensorflow.python.keras.models import Model
-from tensorflow.python.keras.models import Sequential
-from tensorflow.python.keras.layers import Conv2D, Dense, Flatten, Cropping2D, Lambda, Dropout
-
 
 assert hasattr(tf, "function") # Be sure to use tensorflow 2.0
 DATA_PATH = "data/"
@@ -43,7 +39,6 @@ def get_data(batch_size,imageData, rotationData):
 
     for index in range(0,batch_size):
         i = random.choice([0, 1, 2]) # for direction
-
         img = cv2.imread(os.path.join(DATA_IMG,imageData[index][i]).replace(" ", ""))
         if img is None: 
             continue
@@ -64,7 +59,6 @@ def get_data(batch_size,imageData, rotationData):
         rotations.append(rotation)
 
     print("end loading longeur image:",len(images))
-
     return np.array(images), np.array(rotations)
 
 # création du reseaux convolutif
@@ -99,10 +93,9 @@ class ConvModel(keras.Model):
 
         # Flatten the convolution
         self.flatten = keras.layers.Flatten(name="flatten")
-        # Add layers
+        # Add full layers
         self.d1 = keras.layers.Dense(4096, activation='relu', name="d1")
         self.d2 = keras.layers.Dense(4096, activation='relu', name="d2")
-        self.d3 = keras.layers.Dense(1000, activation='relu', name="d3")
         self.out = keras.layers.Dense(1,activation='sigmoid',  name="output")
         
 
@@ -134,12 +127,8 @@ class ConvModel(keras.Model):
         flatten = self.flatten(pool5)
         d1 = self.d1(flatten)
         d2 = self.d2(d1)
-        d3 = self.d3(d2)
-        output = self.out(d3)
+        output = self.out(d2)
         return output
-
-
-
 
 
 data = load_data()
@@ -152,8 +141,8 @@ y_train = data[2]
 y_valid = data[3]
 
 # Peut etre a retirer next
-images, rotations = get_data(32,x_train,y_train)
-images_valid, rotations_valid = get_data(32,x_valid,y_valid)
+images, rotations = get_data(128,x_train,y_train)
+images_valid, rotations_valid = get_data(128,x_valid,y_valid)
 
 # conversion des images de float 64 en 32 car con2d veut du 32
 images = images.astype(np.float32)
@@ -161,17 +150,12 @@ images = images.astype(np.float32)
 images_valid = images_valid.astype(np.float32)
     
 plt.title(rotations[10])
-plt.imshow(images[10], cmap="gray")
+plt.imshow(images[10]/255)
 plt.show()
 
-images=np.array(images)
-rotations = np.array(rotations)
 print("rotation shape",rotations.shape)
-images_valid=np.array(images_valid)
-print("image before shape",images.shape)
-images = np.reshape(images,(-1, 160,320,3))
-images_valid = np.reshape(images_valid,(-1, 160,320,3))
-print("image after shape",images.shape)
+print("image shape",images.shape)
+print("image validation shape",images_valid.shape)
 
 # Normalisation
 print("Moyenne et ecart type des images", images.mean(), images.std())
@@ -179,21 +163,15 @@ scaler = StandardScaler()
 scaled_images = scaler.fit_transform(images.reshape(-1, 160*320))
 scaled_images_valid = scaler.transform(images_valid.reshape(-1, 160*320))
 print("Moyenne et ecart type des images normalisé", scaled_images.mean(), scaled_images.std())
-print("after normalisation",rotations.shape)
-print("after normalisation",scaled_images.shape)
 
 scaled_images = scaled_images.reshape(-1, 160,320,3)
 scaled_images_valid = scaled_images_valid.reshape(-1, 160,320,3)
-print("after scaled",rotations.shape)
-print("after scaled",scaled_images.shape)
+print("scaled images after normalisation",scaled_images.shape)
 
 train_dataset = tf.data.Dataset.from_tensor_slices((scaled_images, rotations))
 valid_dataset = tf.data.Dataset.from_tensor_slices((scaled_images_valid, rotations_valid))
 
 model = ConvModel()
-model.predict(scaled_images[0:1])
-
-BATCH_SIZE = 32
 
 #loss_object = keras.losses.binary_crossentropy()
 optimizer = tf.keras.optimizers.Adam()
@@ -202,10 +180,8 @@ optimizer = tf.keras.optimizers.Adam()
 train_loss = metrics.Mean(name='train_loss')
 valid_loss = metrics.Mean(name='valid_loss')
 # Accuracy
-train_accuracy = metrics.SparseCategoricalAccuracy(name='train_accuracy')
-valid_accuracy = metrics.SparseCategoricalAccuracy(name='valid_accuracy')
-
-
+train_accuracy = metrics.Accuracy(name='train_accuracy')
+valid_accuracy = metrics.Accuracy(name='valid_accuracy')
 
 @tf.function
 def train_step(image, rotations):
@@ -213,35 +189,35 @@ def train_step(image, rotations):
     with tf.GradientTape() as tape:
         # fait une prediction
         predictions = model(image)
-        #tf.reshape(rotations, [1])
-        #tf.reshape(predictions, [1])
-        print("after creation model rotations shape",rotations)
-        print("after creation model prediction shape",predictions)
+        print("rotations shape after creation model",rotations)
+        print("prediction shape after creation model",predictions)
         # calcul de l'erreur en fonction de la prediction et des targets
         loss = keras.losses.mean_squared_error(rotations, predictions)
+        print("calcul loss",loss)
     # calcul du gradient en fonction du loss
     # trainable_variables est la lst des variable entrainable dans le model
     gradients = tape.gradient(loss, model.trainable_variables)
+    print("calcul gradient")
     # changement des poids grace aux gradient
     optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+    print("etape optimizer")
     # ajout de notre loss a notre vecteur de stockage
     train_loss(loss)
+    print("etape train loss")
     train_accuracy(rotations, predictions)
+    print("etape train accuracy")
 
 @tf.function
 # vérifier notre accuracy de notre model afin d'evité l'overfitting
 def valid_step(image, rotations):
-    print("validstep")
     predictions = model(image)
-    #tf.reshape(rotations, [1])
-    #tf.reshape(predictions, [1])
     t_loss = keras.losses.mean_squared_error(rotations, predictions)
     # mets a jour les metrics
     valid_loss(t_loss)
     valid_accuracy(rotations, predictions)
         
 
-epoch = 3
+epoch = 4
 batch_size = 32
 actual_batch = 0
 
@@ -270,4 +246,3 @@ for epoch in range(epoch):
 
 
 model.save("model.h5")
-
